e6927f108df757d56304ac77cec873ce26476249
kangyheona <43988176+kangyheona@users.noreply.github.com>
2019-07-12 13:45:01 +0900

    Delete main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-		$(".tab ul li").click(function(){
-		  var index = $(this).index(); console.log(index);
-		  $(".tab ul li").removeClass("tab_on");
-		  $(this).addClass("tab_on");
-			$(".tab_content").eq(index).show().siblings(".tab_content").hide();
-		});
-	})
